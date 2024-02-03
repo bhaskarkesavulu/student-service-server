8e39b99b3b6201fc5dc0b357e848cff7a17f25f8
bhaskarkesavulu <bhaskar120689@gmail.com>
2024-02-03 21:02:54 +0530

    1st Commit

---
+++ src/main/java/com/bhaskar/studentserviceserver/StudentServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.bhaskar.studentserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceServerApplication.class, args);
+	}
+
+}
